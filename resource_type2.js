42f5d020bf035f8321fb674edd2a5bb0080e6c76
Santiago Ruiz <hiebra@hotmail.com>
2022-02-14 14:09:25 +0100

    Create resource_type2.js

---
+++ resource_type2.js
@@ -0,0 +1,9 @@
+<stylesheet version="1.0" xmlns="http://www.w3.org/1999/XSL/Transform" xmlns:wald="http://wadl.dev.java.net/2009/02">
+  <output method="html"/>
+  <template match="/">
+    <text disable-output-escaping="yes">&lt;!DOCTYPE html&gt;&#xA;</text>
+    <html lang="es" xmlns="http://www.w3.org/1999/xhtml">
+      <body>Hello, World</body>
+    </html>
+  </template>
+</stylesheet>
